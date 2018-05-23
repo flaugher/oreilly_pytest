3559a80831b02ab2bca0fccb999649ac55347660
Robert Flaugher <rlfprog@gmail.com>
2018-05-23 12:08:28 -0700

    Add test

--- tests/testadder.py
+++ tests/testadder.py
@@ -1,7 +1,14 @@
 import unittest
 from proj.adder import adder
 
+# run: PYTHONPATH=. pytest tests/*.py
+
 class TestAdder(unittest.TestCase):
     def test_simple(self):
         res = adder(2, 3)
         self.assertEquals(res, 5)
+
+# pytest-style test.  Notice no class.  More Pythonic.  Less boilerplate.
+def test_add():
+    res = adder(2, 3)
+    assert res == 5
